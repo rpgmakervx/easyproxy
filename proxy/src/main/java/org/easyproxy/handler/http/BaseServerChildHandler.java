83484264b5f9f79b18a3da8609a8c531b929ed89
rpgmakervx <583110127@qq.com>
2016-08-21 22:52:40 +0800

    ip filter

--- proxy/src/main/java/org/easyproxy/handler/http/BaseServerChildHandler.java
+++ proxy/src/main/java/org/easyproxy/handler/http/BaseServerChildHandler.java
@@ -8,6 +8,12 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.*;
+import io.netty.handler.ipfilter.IpFilterRuleType;
+import io.netty.handler.ipfilter.IpSubnetFilterRule;
+import io.netty.handler.ipfilter.RuleBasedIpFilter;
+import org.easyproxy.util.Config;
+
+import java.util.List;
 
 /**
  * Description :
@@ -16,18 +22,30 @@ import io.netty.handler.codec.http.*;
  */
 
 public class BaseServerChildHandler extends ChannelInitializer<SocketChannel> {
+
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
+        System.out.println("客户端请求："+ch.remoteAddress().getHostString());
         ChannelPipeline pipeline = ch.pipeline();
         pipeline.addLast("decoder", new HttpRequestDecoder());
         pipeline.addLast("encoder", new HttpResponseEncoder());
         pipeline.addLast("compress", new HttpContentCompressor(9));
         pipeline.addLast("decompress", new HttpContentDecompressor());
         pipeline.addLast("aggregator", new HttpObjectAggregator(1024000));
+        pipeline.addLast("ipfilter",new RuleBasedIpFilter(getForbiddenList()));
         pipeline.addLast(new AccessLogHandler());
         pipeline.addLast(new AntiLeechHandler());
         pipeline.addLast(new GetRequestHandler());
         pipeline.addLast(new PostRequestHandler());
 
+    }
+
+    private IpSubnetFilterRule[] getForbiddenList(){
+        List<String> forbidden_hosts = Config.getForbiddenHosts();
+        IpSubnetFilterRule [] rules = new IpSubnetFilterRule[forbidden_hosts.size()];
+        for (int index=0;index<forbidden_hosts.size();index++){
+            rules[index] = new IpSubnetFilterRule(forbidden_hosts.get(index),32, IpFilterRuleType.REJECT);
+        }
+        return rules;
     }
 }
