d1faeb70cf1aa8d1a633f49fa29040f8d9218960
rpgmakervx <583110127@qq.com>
2016-11-11 01:50:09 +0800

    multi config file

--- proxy/src/main/java/org/easyproxy/startup/EasyProxy.java
+++ proxy/src/main/java/org/easyproxy/startup/EasyProxy.java
@@ -4,7 +4,6 @@ package org.easyproxy.startup;/**
  *  下午2:28
  */
 
-import org.easyproxy.config.ConfigEnum;
 import org.easyproxy.config.ConfigFactory;
 import org.easyproxy.init.AppInit;
 import org.easyproxy.server.ProxyServer;
@@ -32,7 +31,6 @@ public class EasyProxy {
         System.out.println("easyproxy.home --> "+System.getProperty("easyproxy.home"));
         System.out.println("config path-->"+ConfigFactory.getConfig().getConfigPath());
         ProxyServer server = new ProxyServer();
-        System.out.println("负载均衡策略:"+ ConfigFactory.getConfig().getString(ConfigEnum.LB_STRATEGY.key));
         server.startup();
     }
 
